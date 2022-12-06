1cc06e9c3269ce0779bf4cb7cd7ca0c8d8682b9f
wzl <1920731406@qq.com>
2022-12-06 16:35:42 +0800

    35

---
+++ src/test/java/com/wangzhiliang/data/datastructure/offer35/Solution.java
@@ -0,0 +1,25 @@
+package com.wangzhiliang.data.datastructure.offer35;
+
+public class Solution {
+
+    public Node copyRandomList(Node head) {
+
+
+
+    }
+
+
+   static class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+
+}
