65a01c2b8f92f77ad581e566708a8c35a55dee75
wzl <1920731406@qq.com>
2022-12-07 20:15:30 +0800

    35

--- src/test/java/com/wangzhiliang/data/datastructure/leet217/Solution.java
+++ src/test/java/com/wangzhiliang/data/datastructure/leet217/Solution.java
@@ -1,13 +1,19 @@
 package com.wangzhiliang.data.datastructure.leet217;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
 
     public boolean containsDuplicate(int[] nums) {
 
-
+        Set<Integer> set =new HashSet<>();
+        for(int x: nums){
+            if(!set.add(x)){
+                return  true;
+            }
+        }
         return false;
-
     }
-
 
 }
---
+++ src/test/java/com/wangzhiliang/data/datastructure/leet53/Solution.java
@@ -0,0 +1,10 @@
+package com.wangzhiliang.data.datastructure.leet53;
+
+public class Solution {
+    public int maxSubArray(int[] nums) {
+
+
+
+
+    }
+}
